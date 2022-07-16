c070d01fd3ee012ce1eda61d97a4658631c0daf4
Brian Giori <brian.giori@amplitude.com>
2022-07-15 18:20:31 -0700

    remove unecessary flag config fields

--- evaluation-core/src/commonMain/kotlin/MutualExclusionConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.amplitude.experiment.evaluation
-
-data class MutualExclusionConfig(
-    val groupSalt: String,
-    val lowerBound: Int,
-    val percentage: Int,
-    val bucketingKey: String = DEFAULT_BUCKETING_KEY,
-)
