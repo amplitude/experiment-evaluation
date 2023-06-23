2a69b7619280b8ab7cf3316460372d5f10417fa6
Brian Giori <brian.giori@amplitude.com>
2023-06-23 14:03:19 -0700

    sign publication

--- evaluation-core/build.gradle.kts
+++ evaluation-core/build.gradle.kts
@@ -83,14 +83,13 @@ publishing {
     }
 }
 
-// signing {
-//     val publishing = extensions.findByType<PublishingExtension>()
-//     val signingKeyId = System.getenv("SIGNING_KEY_ID")
-//     val signingKey = System.getenv("SIGNING_KEY")
-//     val signingPassword = System.getenv("SIGNING_PASSWORD")
-//     useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-//     sign(publishing?.publications)
-// }
+signing {
+    val publishing = extensions.findByType<PublishingExtension>()
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing?.publications)
+}
 
 tasks.withType<Sign>().configureEach {
     onlyIf { isReleaseBuild }
