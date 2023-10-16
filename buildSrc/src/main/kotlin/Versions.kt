bbcfed5d8f307953372c8201594f501b716f8d6d
Brian Giori <brian.giori@amplitude.com>
2023-10-16 15:37:55 -0700

    update kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val serializationPlugin = "1.9.0"
-    const val serializationRuntime = "1.6.0-RC"
+    const val serializationRuntime = "1.6.0"
     const val kotlinLint = "11.5.1"
 
     // Testing
